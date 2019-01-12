a11e4574710b5b460ed9b943ae27a9dd2a0e875f
Vitaliy <theyamshikov@yandex.ru>
2019-01-12 16:52:05 +0600

    refacto

--- src/domain/repositories/filedb/TestRepository.php
+++ src/domain/repositories/filedb/TestRepository.php
@@ -2,10 +2,10 @@
 
 namespace yii2lab\notify\domain\repositories\filedb;
 
-use yii2lab\domain\repositories\ActiveDiscRepository;
+use yii2lab\extension\arrayTools\repositories\base\BaseActiveDiscRepository;
 use yii2lab\notify\domain\interfaces\repositories\TestInterface;
 
-class TestRepository extends ActiveDiscRepository implements TestInterface {
+class TestRepository extends BaseActiveDiscRepository implements TestInterface {
 	
 	public $table = 'notify_test';
 	public $path = '@common/runtime/data';
